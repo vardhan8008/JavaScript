235375230d60e21313e6554d0ef82e8194642468
vardhan8008 <65539714+vardhan8008@users.noreply.github.com>
2023-10-21 15:01:16 +0000

    data types

---
+++ 01_Basics/03_dataconversions.js
@@ -0,0 +1,23 @@
+//*************************operations************************ *
+let value=4;
+let negvalue=-value;
+console.log(value);
+console.log(negvalue);
+console.log(3+negvalue);
+console.log(3%2);
+
+let str1="hello"
+let str2=" vardhan Reddy"
+let str3=str1+str2
+console.log(str3)
+
+//type conversion
+console.log(1+"2")
+console.log("2"+1+2)
+
+console.log(2+1+"2")
+console.log("2"+(3+1))
+console.log(+true)
+console.log(+"")
+console.log(+false)
+
