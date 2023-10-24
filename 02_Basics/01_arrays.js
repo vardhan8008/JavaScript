499e2a3d4d698f355151c9ad12f4efddd90c24c1
vardhan8008 <65539714+vardhan8008@users.noreply.github.com>
2023-10-24 14:37:24 +0000

    arrays

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -1,3 +1,43 @@
 //Arrays ==> collection of data
 
-const myArray = []
+// const myArray = [100,200,300,400]
+// console.log(myArray[3])
+
+// const myArray1=new Array(1,2,3,4)
+// console.log(myArray1[0])
+// console.log(myArray1)
+
+// //array Methods
+
+// // myArray.push(600)
+// // myArray.push(900)
+// // myArray.pop()
+// myArray.unshift(78)
+// console.log(myArray)
+// myArray.shift()
+// console.log(myArray)
+
+// console.log(myArray.includes(300))
+
+// console.log(myArray.indexOf(300))
+
+// const newArr=myArray.join()
+
+// console.log(myArray)
+// console.log(newArr)
+// console.log(typeof(newArr))
+
+
+//slice and splice
+const myArray = [100,200,300,400,500]
+const sclice1=myArray.slice(1,3)
+console.log(sclice1)
+
+
+//splice
+
+splice1=myArray.splice(1,3)
+console.log(splice1)
+console.log(myArray)
+
+
