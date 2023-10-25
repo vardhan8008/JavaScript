a998b5a4e25b9966f60afde141ee013bd45af82c
vardhan8008 <65539714+vardhan8008@users.noreply.github.com>
2023-10-25 15:26:03 +0000

    functions

---
+++ 02_Basics/05_functions.js
@@ -0,0 +1,36 @@
+// function Display(){
+//     console.log("Vardhan")
+// }
+// Display()
+// // console.log(x.Display())
+
+// let y=function (num1,num2){
+//     console.log(num1+num2)
+
+// }
+// console.log(y(3,5))
+
+// function mulTwo(num3,num4){
+//     return (num3*num4)
+// }
+// let res=mulTwo(5,6)
+// console.log(res)
+
+// function greet(name){
+//     return `welcome! ${name}.`
+// }
+// console.log(greet("Vardhan"))
+
+let Details={
+    username:"Raju",
+    age:23,
+    location:"Hyderabad"
+}
+
+function handleObjects(anyObject){
+    console.log( `Hii ${anyObject.username} welcome to ${anyObject.location}!`)
+}
+handleObjects({username:"Vardhan",age:23,location:"Bengaluru"})
+
+
+
